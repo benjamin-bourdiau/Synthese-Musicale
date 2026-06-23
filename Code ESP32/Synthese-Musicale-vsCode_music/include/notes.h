34ab02c7ee368661ebb0ea1ee68a0a05960dd7a4
PRUVOST Fabien <fabien.pruvost@geii2.iut-cachan.u-psud.fr>
2026-06-23 10:34:20 +0200

    Ajout projet VSCode

---
+++ Code ESP32/Synthese-Musicale-vsCode_music/include/notes.h
@@ -0,0 +1,34 @@
+/*
+ * Fichier : notes.h
+ * Description : Librairie des fréquences pour la 2ème et 3ème Octaves
+ */
+
+#ifndef NOTES_H
+#define NOTES_H
+
+const float NOTE_DO_2    = 126.8;
+const float NOTE_DO_D_2  = 134.6;
+const float NOTE_RE_2    = 142.8;
+const float NOTE_RE_D_2  = 151.6;
+const float NOTE_MI_2    = 159.8;
+const float NOTE_FA_2    = 169.6;
+const float NOTE_FA_D_2  = 180.0;
+const float NOTE_SOL_2   = 191.0; 
+const float NOTE_SOL_D_2 = 201.6;
+const float NOTE_LA_2    = 214.0;
+const float NOTE_LA_D_2  = 227.1;
+const float NOTE_SI_2    = 240.9;
+const float NOTE_DO_3    = 253.6; 
+const float NOTE_DO_D_3  = 269.2;
+const float NOTE_RE_3    = 285.7;
+const float NOTE_RE_D_3  = 302.1;
+const float NOTE_MI_3    = 319.6;
+const float NOTE_FA_3    = 339.2;
+const float NOTE_FA_D_3  = 360.0;
+const float NOTE_SOL_3   = 382.0;
+const float NOTE_SOL_D_3 = 404.3;
+const float NOTE_LA_3    = 428.0;
+const float NOTE_LA_D_3  = 452.2;
+const float NOTE_SI_3    = 478.9;
+
+#endif // NOTES_H
